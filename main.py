178c751e7d6249db5d1b346c3763a6036ba8add9
a-turophile <65869103+a-turophile@users.noreply.github.com>
2020-09-02 15:12:19 +0530

    Updated main file

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+from add import add
+from subtract import subtract
+from multiply import multiply
+from divide import divide
 
 print("***CALCULATOR***")
 a = int(input("Enter num1: "))
 b= int(input("Enter num2: "))
 
-ch = int(input("Enter your choice: "))
+ch = int(input("Enter your choice:\n\t1. Addition \n\t2. Subtraction \n\t3. Multiplication \n\t4. Division \n"))
 if(ch==1):
-    #return addition
+    print("\nThe sum of numbers is: ",add(a,b))
 elif(ch==2):
-    #return subtraction
+    print("\nThe difference of numbers is: ",subtract(a,b))
 elif(ch==3):
-    #return multiplication
+    print("\nThe product of numbers is: ",multiply(a,b))
 elif(ch==4):
-    #return division
+    print("\nDivision results in: ",divide(a,b))
 else:
-    print("Please choose a correct option")
+    print("\n\t\t\tPlease choose a correct option")
 
