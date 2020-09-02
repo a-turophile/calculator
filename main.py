7c75bd0a4ba41abc52ee97e576666a802ca43d20
a-turophile <65869103+a-turophile@users.noreply.github.com>
2020-09-02 14:10:05 +0530

    Main file

---
+++ main.py
@@ -0,0 +1,17 @@
+
+print("***CALCULATOR***")
+a = int(input("Enter num1: "))
+b= int(input("Enter num2: "))
+
+ch = int(input("Enter your choice: "))
+if(ch==1):
+    #return addition
+elif(ch==2):
+    #return subtraction
+elif(ch==3):
+    #return multiplication
+elif(ch==4):
+    #return division
+else:
+    print("Please choose a correct option")
+
